62973dc5359c2291263bb69a721a4ed4a026936c
User <User@LAPTOP-A4KPS0K4>
2024-06-03 18:00:39 +0300

    task1 part1

---
+++ Banckapp/BanckAppMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_hajyahia_yazan.Banckapp
+{
+    internal class BanckAppMain
+    {
+        public static void MainEntry()
+        {
+            Console.WriteLine("BanckApp");
+        }
+    }
+}
