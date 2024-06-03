b05d73654f04858b20b79cfd9713eb2352ef6fd9
User <User@LAPTOP-A4KPS0K4>
2024-06-03 17:54:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_hajyahia_yazan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
